e7caf1a3167226bd3f4cf3cda2be3102e9aeb2ec
Tran Nguyen <trannguyen61st@gmail.com>
2020-12-06 23:04:17 +0700

    authenticated navigation bar display

--- components/CustomNavbar.vue
+++ components/CustomNavbar.vue
@@ -7,72 +7,114 @@
       EzAccomod
     </nuxt-link>
 
-    <button
-      v-if="$vuetify.breakpoint.mdAndUp"
-      v-ripple
-      class="custom-btn custom-btn--text mx-5"
-      @click="$router.push('/register')"
-    >
-      <nuxt-link
-        to="/register"
-        class="link-text"
+    <!-- Button for all screens + Logged in -->
+    <template v-if="loggedIn">
+      <v-btn
+        icon
+        class="mr-6"
       >
-        Đăng ký
-      </nuxt-link>
-    </button>
-    <button
-      v-if="$vuetify.breakpoint.mdAndUp"
-      v-ripple
-      class="custom-btn custom-btn--text custom-btn__densed"
-    >
-      Đăng nhập
-    </button>
-
-    <v-icon
-      v-if="$vuetify.breakpoint.smAndDown && sidebar"
-      color="primary"
-      @click="$emit('toggle-navbar')"
-    >
-      fas fa-bars
-    </v-icon>
-
-    <v-menu 
-      v-if="$vuetify.breakpoint.smAndDown && !sidebar" 
-      offset-y
-      transition="slide-y-transition"
-      min-width="120px"
-    >
-      <template #activator="{ on, attrs }">
-        <v-btn 
-          v-bind="attrs"
-          icon 
-          v-on="on" 
-          @click="menu = !menu"
+        <v-badge
+          :content="notif"
+          :value="notif"
+          color="primary"
+          overlap
         >
-          <v-icon color="primary">
-            fas fa-bars
+          <v-icon>
+            fas fa-bell
           </v-icon>
-        </v-btn>
-      </template>
-      <v-list class="d-flex flex-column">
-        <button
-          v-ripple
-          class="custom-btn custom-btn--text"
+        </v-badge>
+      </v-btn>
+    </template>
+
+    <!-- Button for medium and up screens + Logged in -->
+    <template v-if="$vuetify.breakpoint.mdAndUp && loggedIn">
+      <v-btn
+        to="/user"
+        nuxt
+        icon
+      >
+        <v-icon>fas fa-user</v-icon>
+      </v-btn>
+    </template>
+
+    <!-- Button group for medium and up screens + Not logged in yet -->
+    <template v-if="$vuetify.breakpoint.mdAndUp && !loggedIn">
+      <button
+        v-ripple
+        class="custom-btn custom-btn--text mx-5"
+        @click="$router.push('/register')"
+      >
+        <nuxt-link
+          to="/register"
+          class="link-text"
         >
           Đăng ký
-        </button>
-        <button
-          v-ripple
-          class="custom-btn custom-btn--text custom-btn__secondary"
-        >
-          Đăng nhập
-        </button>
-      </v-list>
-    </v-menu>
+        </nuxt-link>
+      </button>
+      <button
+        v-ripple
+        class="custom-btn custom-btn--text custom-btn__densed"
+      >
+        Đăng nhập
+      </button>
+    </template>
+
+    <!-- Hamburger toggle navigation bar for small and down screen - Excluding sidebar -->
+    <template v-if="$vuetify.breakpoint.smAndDown && sidebar">
+      <v-icon
+        color="primary"
+        @click="$emit('toggle-navbar')"
+      >
+        fas fa-bars
+      </v-icon>
+    </template>
+
+    <!-- Hamburger toggle navigation bar for small and down screen - Including sidebar -->
+    <template v-if="$vuetify.breakpoint.smAndDown && !sidebar">
+      <v-menu 
+        offset-y
+        transition="slide-y-transition"
+        min-width="120px"
+      >
+        <template #activator="{ on, attrs }">
+          <v-btn 
+            v-bind="attrs"
+            icon 
+            v-on="on" 
+            @click="menu = !menu"
+          >
+            <v-icon color="primary">
+              fas fa-bars
+            </v-icon>
+          </v-btn>
+        </template>
+
+        <template v-if="!loggedIn">
+          <v-list class="d-flex flex-column">
+            <button
+              v-ripple
+              class="custom-btn custom-btn--text"
+            >
+              Đăng ký
+            </button>
+            <button
+              v-ripple
+              class="custom-btn custom-btn--text custom-btn__secondary"
+            >
+              Đăng nhập
+            </button>
+          </v-list>
+        </template>
+
+        <template v-else />
+      </v-menu>
+    </template>
   </nav>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   props: {
     sidebar: {
@@ -83,8 +125,15 @@ export default {
 
   data () {
     return {
-      menu: false
+      menu: false,
+      notif: 1
     }
   },
+
+  computed: {
+    ...mapGetters({
+      loggedIn: 'user/loggedIn'
+    })
+  }
 }
 </script>
